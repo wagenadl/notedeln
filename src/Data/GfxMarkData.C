b963aba492ead690a6f55353d08ed557b85986c5
Daniel Wagenaar <daw@caltech.edu>
2012-11-05 14:50:50 -0800

    doodledata and markdata are for sketched polylines and individual points, respectively.

---
+++ src/Data/GfxMarkData.C
@@ -0,0 +1,42 @@
+// GfxMarkData.C
+
+#include "GfxMarkData.H"
+
+static Data::Creator<GfxMarkData> c("gfxmark");
+
+GfxMarkData::GfxMarkData(Data *parent): GfxData(parent) {
+  col = QColor("black");
+  siz = 5;
+  shp = SolidCircle;
+}
+
+GfxMarkData::~GfxMarkData() {
+}
+
+QColor GfxMarkData::color() const {
+  return col;
+}
+    
+double GfxMarkData::size() const {
+  return siz;
+}
+
+GfxMarkData::Shape GfxMarkData::shape() const {
+  return shp;
+}
+
+void GfxMarkData::setColor(QColor c) {
+  col = c;
+  markModified();
+}
+
+void GfxMarkData::setSize(double s) {
+  siz = s;
+  markModified();
+}
+
+void GfxMarkData::setShape(Shape s) {
+  shp = s;
+  markModified();
+}
+
