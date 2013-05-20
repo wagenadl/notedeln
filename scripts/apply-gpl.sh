d2d1ebb5dd0ac39e8350f0f2e9633b5841436aa5
Daniel Wagenaar <daw@caltech.edu>
2013-05-20 15:25:26 -0700

    Started writing a user guide.

---
+++ scripts/apply-gpl.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+FN=$1
+
+grep -q "This file is part of eln" && exit 0
+
+/bin/mv $FN $FN~
+echo "// $FN - This file is part of eln" > $FN
+
+cat >> $FN <<EOF
+
+/* eln is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   eln is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with eln.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+EOF
+
+cat $FN~ >> $FN
