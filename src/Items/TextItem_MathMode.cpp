cc572100beec0da791910d02b13e5c106c0dd0f1
Daniel Wagenaar <wagenadl@uc.edu>
2014-03-14 16:09:01 -0400

    Starting on MathMode

---
+++ src/Items/TextItem_MathMode.cpp
@@ -0,0 +1,17 @@
+// MathMode.cpp
+
+#include "TextItem.H"
+#include <QKeyEvent>
+
+#if 0
+bool TextItem::keyPressMath(QKeyEvent *e) {
+  int key = e->key();
+  Qt::KeyboardModifiers mod = e->modifiers();
+  QString txt = e->text();
+  switch (key) {
+  default:
+  }
+  return false;
+}
+#endif
+ 
