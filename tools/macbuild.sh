9e7f04fe0c1c1eea325b25c86db0526b4212ed04
Daniel Wagenaar <wagenadl@uc.edu>
2015-06-01 15:57:27 -0400

    Cleanup of the tools directory

--- tools/macbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -rf eln.app eln.dmg
-make -C src release
-make -C webgrab release
-make -C repairtoc release
-cp webgrab/webgrab.app/Contents/MacOS/webgrab eln.app/Contents/MacOS
-cp repairtoc/eln-repairtoc.app/Contents/MacOS/eln-repairtoc eln.app/Contents/MacOS
