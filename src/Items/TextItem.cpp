38707a711900f2a8635f103f408af0e4032d29c7
Daniel A. Wagenaar <daw@caltech.edu>
2019-02-01 10:02:24 -0800

    .

--- src/Items/TextItem.cpp
+++ src/Items/TextItem.cpp
@@ -1403,7 +1403,7 @@ QRectF TextItem::boundingRect() const {
 void TextItem::paint(QPainter *p, const QStyleOptionGraphicsItem *, QWidget *) {
   if (!text)
     return;
-
+  
   if (clips())
     p->setClipRect(clip_.adjusted(-10,0,10,0));
   else
