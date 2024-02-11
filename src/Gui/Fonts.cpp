032a7c50d6ad6189f39a292d26e4cd8f2b229927
Daniel A. Wagenaar <daw@caltech.edu>
2024-02-10 17:38:48 -0800

    Imported original Ubuntu font as UbuntuOrigWeight

--- src/Gui/Fonts.cpp
+++ src/Gui/Fonts.cpp
@@ -24,14 +24,14 @@
 
 Fonts::Fonts() {
   QStringList ff;
-  ff << ":fonts/Ubuntu-BI.ttf";
-  ff << ":fonts/Ubuntu-B.ttf";
-  ff << ":fonts/Ubuntu-RI.ttf";
-  ff << ":fonts/Ubuntu-R.ttf";
+  ff << ":fonts/UbuntuOrigWeight-BI.ttf";
+  ff << ":fonts/UbuntuOrigWeight-B.ttf";
+  ff << ":fonts/UbuntuOrigWeight-RI.ttf";
+  ff << ":fonts/UbuntuOrigWeight-R.ttf";
   ff << ":fonts/NotoSansMath-Regular.ttf";
 
   foreach (QString f, ff)
     QFontDatabase::addApplicationFont(f);
 
-  QFont::insertSubstitution("Ubuntu", "Noto Sans Math");
+  QFont::insertSubstitution("UbuntuOrigWeight", "Noto Sans Math");
 }
